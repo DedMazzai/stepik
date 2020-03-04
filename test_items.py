0438669001815bdcff88a9f7e1ef3daddcfce23d
dedmazzai <ded_mazzai@inbox.ru>
2020-03-04 15:43:44 +0300

    add try - exept

--- test_items.py
+++ test_items.py
@@ -3,8 +3,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 def test_presence_of_button(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207")
-    time.sleep(0)
-    assert WebDriverWait(browser, 5).until(EC.presence_of_element_located(
-        (By.CSS_SELECTOR, "#add_to_basket_form button")))
+    time.sleep(30)
+    try:
+        assert WebDriverWait(browser, 5).until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, "#add_to_basket_form button")))
+        print("<<Item found!>>")
+    except:
+        print("<<O-Oh! Item not found!!!>>")
